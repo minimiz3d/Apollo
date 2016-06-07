9394bf3072a2219698bc087a62dc9000985f6357
minimiz3d <arthur-357@hotmail.com>
2016-06-07 19:03:33 -0300

    Add files via upload

---
+++ GuitarTuner/guitarTunerFunctions.c
@@ -0,0 +1,10 @@
+#include "guitarTuner.h"
+
+/* Definir corda a ser analisada */
+void selectString() {}
+
+/* Definir modo de operação: afinação ou aprendizado */
+void selectMode() {}
+
+/* Acionamento do motor */
+void servoSetting() {}
