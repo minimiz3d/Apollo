35609aea7c1a8f95f36856e6284b42450d0342fd
minimiz3d <arthur-357@hotmail.com>
2016-06-07 18:38:40 -0300

    Add files via upload

---
+++ GuitarTuner/main-1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "guitarTuner.h"
+
+/* Estados da tune_FSM */
+typedef enum {
+    ST_1, ST_2, ST_3, ST_4, ST_5, ST_6, ST_7, ST_8
+} tuneStates;
+
+/* Estados da learn_FSM */
+typedef enum {
+    SL_1, SL_2, SL_3, SL_4, SL_5, SL_6, SL_8
+} learnStates;
+
+/* Ponteiro de função para um determinado estado */
+typedef void (*Action)(unsigned char algo); //...
+
+/* Dados e ponteiros de funções */
+typedef struct FiniteStateMachine {
+    tuneStates TS;
+    learnStates LS;
+    Action action[8];
+} FSM;
