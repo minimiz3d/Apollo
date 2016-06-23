5e486d00a336da05bfb42f0f6150be2cba6ee388
Gabriel <gabrieldejesus2211@gmail.com>
2016-06-23 15:23:32 -0300

    Main revised

--- GuitarTuner/main.c
+++ GuitarTuner/main.c
@@ -3,6 +3,13 @@
 
 int main()
 {
+        // Supondo selectMode() = afinação:
+
+    /* Leitura/conversão do sinal analógico em digital */
+    inputWave();
+
+    /* Com base na representação digital do sinal obtido anteriormente, faz-se a FFT */
+    defineFrequency();
     while(1)
     {
         // Do something
