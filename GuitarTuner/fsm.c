f4620f082213561ccff95c7c1136c267ea01f444
minimiz3d <arthur-357@hotmail.com>
2016-06-25 13:11:51 -0300

    FUNÇÕES DE ESTADO fsm.c

--- GuitarTuner/fsm.c
+++ GuitarTuner/fsm.c
@@ -29,3 +29,23 @@ typedef struct String {
     char stringName;
     uint8_t tunedFrequency;
 } STRING;
+
+/* Funções de estados de afinação */
+void st1();
+void st2();
+void st3();
+void st4();
+void st5();
+void st6();
+void st7();
+void st8();
+
+/* Funções de estados de aprendizado */
+void stL1();
+void stL2();
+void stL3();
+void stL4();
+void stL5();
+void stL6();
+void stL7();
+void stL8();
