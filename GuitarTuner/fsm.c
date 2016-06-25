cfea65e3ca90761fa1b5568c480bffc282f2e127
minimiz3d <arthur-357@hotmail.com>
2016-06-25 18:28:51 -0300

    Update fsm.c

--- GuitarTuner/fsm.c
+++ GuitarTuner/fsm.c
@@ -4,33 +4,8 @@
  * This file defines a finite state machine for the tuner
  */
 
-/* Tuning states*/
-typedef enum {
-    ST_INIT, ST_1, ST_2, ST_3, ST_4, ST_5, ST_6;
-} tuneStates;
-
-/* Learning states */
-typedef enum {
-    SL_INIT, SL_1, SL_2, SL_3, SL_4, SL_5, SL_6;
-} learnStates;
-
-/* Ponteiro de função para um determinado estado */
-typedef void (*Action)(unsigned char algo); //...
-
-/* Dados e ponteiros de funções */
-typedef struct FiniteStateMachine {
-    tuneStates TS;
-    learnStates LS;
-    Action action[16];
-} fsm;
-
-/* Atributos de cada corda */
-typedef struct String {
-    char stringName;
-    uint8_t tunedFrequency;
-} STRING;
-
-void initTuneFSM() {
+/* Inicialização das FSMs */
+void initTuneFSM(tfsm fsm) {
     fsm.action[ST_1] = st1;
     fsm.action[ST_2] = st2;
     fsm.action[ST_3] = st3;
@@ -40,7 +15,7 @@ void initTuneFSM() {
     fsm.action[ST_7] = st7;
     fsm.action[ST_8] = st8;
 }
-void initLearnFSM() {
+void initLearnFSM(lfsm fsm) {
     fsm.action[SL_1] = sl1;
     fsm.action[SL_2] = sl2;
     fsm.action[SL_3] = sl3;
@@ -53,52 +28,68 @@ void initLearnFSM() {
 
 /* Funções de estados de afinação */
 void st1() {
+    tuneString();
     fsm.TS = ST_2;  // Avança na FSM.
 }
 void st2() {
+    tuneString();
     fsm.TS = ST_3;  // Avança na FSM.
 }
 void st3() {
+    tuneString();
     fsm.TS = ST_4;  // Avança na FSM.
 }
 void st4() {
+    tuneString();
     fsm.TS = ST_5;  // Avança na FSM.
 }
 void st5() {
+    tuneString();
     fsm.TS = ST_6;  // Avança na FSM.
 }
 void st6() {
+    tuneString();
     fsm.TS = ST_7;  // Avança na FSM.
 }
 void st7() {
+    tuneString();
     fsm.TS = ST_8;  // Avança na FSM.
 }
 void st8() {
+    tuneString();
     fsm.TS = S0;  // Avança na FSM.
 }
 
 /* Funções de estados de aprendizado */
 void sl1() {
+    string[i].tunedFrequency = detectFrequency();
     fsm.LS = SL_2;  // Avança na FSM.
 }
 void sl2() {
+    string[i].tunedFrequency = detectFrequency();
     fsm.LS = SL_3;  // Avança na FSM.
 }
 void sl3() {
+    string[i].tunedFrequency = detectFrequency();
     fsm.LS = SL_4;  // Avança na FSM.
 }
 void sl4() {
+    string[i].tunedFrequency = detectFrequency();
     fsm.LS = SL_5;  // Avança na FSM.
 }
 void sl5() {
+    string[i].tunedFrequency = detectFrequency();
     fsm.LS = SL_6;  // Avança na FSM.
 }
 void sl6() {
+    string[i].tunedFrequency = detectFrequency();
     fsm.LS = SL_7;  // Avança na FSM.
 }
 void sl7() {
+    string[i].tunedFrequency = detectFrequency();
     fsm.LS = SL_8;  // Avança na FSM.
 }
 void sl8() {
+    string[i].tunedFrequency = detectFrequency();
     fsm.LS = S0;  // Avança na FSM.
 }
