5dbef82f51ccf8c74f2ac4fa81bdbd1f713d4d07
minimiz3d <arthur-357@hotmail.com>
2016-06-25 15:30:17 -0300

    Update guitarTuner.h
    [+]Tabelas de frequências e nomes

--- GuitarTuner/guitarTuner.h
+++ GuitarTuner/guitarTuner.h
@@ -1,13 +1,36 @@
 #ifndef STATE_FUNCTIONS
 #define STATE_FUNCTIONS
 
-/* Definir modo de operação */
-void selectMode();
+/* Frequências padrão de afinação */
+float frequencyTable[] = {
+    329.63,   // E4
+    246.94,   // B3
+    196.00,   // G3
+    146.83,   // D3
+    110.00,   // A2
+    82.41     // E2
+};
+
+/* Opcional */
+char stringNames[] = {
+    "E4",
+    "B3",
+    "G3",
+    "D3",
+    "A2",
+    "E2"
+};
+
+/* Atributos de cada corda */
+typedef struct String {
+    char stringName;/* Opcional */
+    float tunedFrequency;
+} STRING;
 
 /* Leitura de porta digital */
 uint8_t readPORTB();
 
-/* Definir modo de operação: afinação ou aprendizado */
+/* Definir modo de operação */
 void selectMode();
 
 /* Acionamento do motor */
@@ -15,5 +38,8 @@ void actMotor();
 
 /* Função genérica de afinamento */
 void tuneString();
+
+/* Inicialização do objeto String com suas respectivas frequências padrão de afinação */
+void defineStrings();
 
 #endif
