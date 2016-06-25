d7641a517c0097fc36089cff4a91fc8986379e85
minimiz3d <arthur-357@hotmail.com>
2016-06-25 13:38:04 -0300

    Update fsm.h

--- GuitarTuner/fsm.h
+++ GuitarTuner/fsm.h
@@ -1,6 +1,24 @@
 #ifndef FSM_H_
 #define FSM_H_
 
-int ();
+/* Funções de estados de afinação */
+void st1();
+void st2();
+void st3();
+void st4();
+void st5();
+void st6();
+void st7();
+void st8();
+
+/* Funções de estados de aprendizado */
+void stL1();
+void stL2();
+void stL3();
+void stL4();
+void stL5();
+void stL6();
+void stL7();
+void stL8();
 
 #endif
