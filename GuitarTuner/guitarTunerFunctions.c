a49d0cf44ea3e6d3b43bcd15935bca6351dded4a
minimiz3d <arthur-357@hotmail.com>
2016-06-25 12:48:08 -0300

    Novas funções
    selectMode + readPort
    tuneString

--- GuitarTuner/guitarTunerFunctions.c
+++ GuitarTuner/guitarTunerFunctions.c
@@ -1,22 +1,28 @@
 #include "guitarTuner.h"
-
-/* Realiza analogRead() com o ADC */
-void inputWave() {
-    // Importar código de config do ADC.
-    // Tá na mão.
-}
-
-/* Obter a frequência de uma determinada nota através de uma FFT */
-void defineFrequency() {
-    // Importar código da FFT.
-    // Tá na mão.
-}
-
-/* Definir corda a ser analisada */
-void selectString() {}
+#include <avr/io.h>
 
 /* Definir modo de operação: afinação ou aprendizado */
-void selectMode() {}
+void selectMode() {
+    mode = readPORTB();
+}
+uint8_t readPORTB() {
+    DDRD = 0x00;    // Porta D - entrada.
+    mode = PORTD;   // Leitura.
+
+    return mode;
+}
 
 /* Acionamento do motor */
 void servoSetting() {}
+
+/* Função genérica de afinamento */
+void tuneString() {
+    currentFrequency = detectFrequencty(); // Atualizar essa variável com a frequência desafinada da corda em questão.
+
+    while (currentFrequency != string[i].frequency) {
+        servoSetting(); // Ainda não sei o quanto é pra girar e tal
+        // Dentro da função servoSetting a gente vai atualizando a "currentFrequency".
+    }
+
+    stringInTune = true;    // Avança na FSM.
+}
