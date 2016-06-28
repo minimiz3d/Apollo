f1472a4e9122bcead9dc26a64b103c2b0ac488d4
minimiz3d <arthur-357@hotmail.com>
2016-06-28 19:25:22 -0300

    Funções necessárias

--- Apollo/tuner.h
+++ Apollo/tuner.h
@@ -1,47 +1,31 @@
-#ifndef TUNER_H_
-#define TUNER_H_
+#ifndef STATE_FUNCTIONS
+#define STATE_FUNCTIONS
 
-/* Frequências padrão de afinação */
-float frequencyTable[6] = {
-    329.63,   // E4
-    246.94,   // B3
-    196.00,   // G3
-    146.83,   // D3
-    110.00,   // A2
-    82.41     // E2
-};
-
-/* Opcional */
-char *stringNames[6] = {
-    "E4",
-    "B3",
-    "G3",
-    "D3",
-    "A2",
-    "E2"
-};
+#include <avr/io.h>
+#include <stdbool.h>
 
 /* Atributos de cada corda */
 typedef struct String {
-    char stringName;/* Opcional */
-    float tunedFrequency;
+	char *stringName;/* Opcional */
+	float tunedFrequency;
 } STRING;
 
-/* Leitura de porta digital */
-uint8_t readPORTB();
-
-/* Definir modo de operação */
-uint8_t selectMode();
+/* Leitura da frequência */
+float detectFrequency();
 
 /* Acionamento do motor */
 void actMotor();
 
 /* Função genérica de afinamento */
-void tuneString(STRING string, uint8_t i);
+void tuneString(STRING *string, uint8_t i);
 
 /* Inicialização do objeto String com suas respectivas frequências padrão de afinação */
-void defineStrings();
+void defineStrings(STRING *string);
 
-uint8_t selectMode();
+/* Seleciona a afinação */
+void selectTuning(uint8_t tuning);
+
+/* Salva afinação (modo de aprendizado) */
+void saveFrequency(float frequency, STRING *string, uint8_t stringNum);
 
 #endif
