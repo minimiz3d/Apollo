ba1464b272672b82bdd91d23c4ad3be835086407
minimiz3d <arthur-357@hotmail.com>
2016-06-28 15:23:17 -0300

    Commented Stuff and Implemented COISAS
    :+1:

--- Apollo/main.c
+++ Apollo/main.c
@@ -1,28 +1,37 @@
 #include <avr/io.h>
 #include "fsm.h"
-#include "tuner.h"
+#include "guitarTuner.h"
 
 int main() {
 	STRING string[6];           // 6 cordas para afinar.
 
-	uint8_t mode = selectMode();        // Selecionar modo: afinação ou aprendizado.
-
-	if (!mode) {                // Afinação.
-		tfsm fsm;
-		initTuneFSM(fsm);       // Inicializa os estados com suas respectivas funções.
-		fsm.TS = ST_1;          // Estado inicial da FSM.
-		fsm.action[fsm.TS]();   // Inicia afinação.
-	}
-
-	else {                      // Aprendizado.
-		lfsm fsm;               // Inicia aprendizado.
-		initLearnFSM(fsm);      // Inicializa os estados com suas respectivas funções.
-		fsm.LS = SL_1;          // Estado inicial da FSM.
-		fsm.action[fsm.LS]();
-	}
-	
 	while(1) {
+		if (PORTA0) { // readPORTA0
+			mode = 1;
+		} else if (PORTA1) {
+			mode = 0; // readPORTA1
+		} else {
+			// uint8_t mode = selectMode();        // Selecionar modo: afina��o ou aprendizado.
+			fsmInit(mode);						// Inicialização da FSM.
+			fsm.state = S0;
+			fsm.action[fsm.state]();			// Define o próximo estado a ser executado (função do estado S0).
+		}
 	}
 
 	return 0;
 }
+
+
+// if (!mode) {                // Afina��o.
+// 	tfsm fsm;
+// 	initTuneFSM(fsm);       // Inicializa os estados com suas respectivas fun��es.
+// 	fsm.TS = ST_1;          // Estado inicial da FSM.
+// 	fsm.action[fsm.TS]();   // Inicia afina��o.
+// }
+//
+// else {                      // Aprendizado.
+// 	lfsm fsm;               // Inicia aprendizado.
+// 	initLearnFSM(fsm);      // Inicializa os estados com suas respectivas fun��es.
+// 	fsm.LS = SL_1;          // Estado inicial da FSM.
+// 	fsm.action[fsm.LS]();
+// }
