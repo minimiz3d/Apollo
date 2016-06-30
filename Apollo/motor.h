519014f45a40e5b6bf33f2260d0ad21c70f3bc6e
minimiz3d <arthur-357@hotmail.com>
2016-06-29 22:31:01 -0300

    Comentários com dúvida......

--- Apollo/motor.h
+++ Apollo/motor.h
@@ -1,3 +1,9 @@
+/** @file motor.h
+ *  @brief Protótipo de funções utilizados para o acionamento do servo motor.
+ *  @author Árthur Pinheiro
+ *  @author Gabriel de Jesus
+ */
+
 #ifndef MOTOR_H_
 #define MOTOR_H_
 
@@ -5,12 +11,24 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
+/**
+ *	@brief Definição de frequência de operação do ATmega328p.
+ */
 #define F_CPU 16000000
 
+/**
+ *	@brief ???
+ */
 void configMotor(void);
 
+/**
+ *	@brief ???
+ */
 void Wait(void);
 
+/**
+ *	@brief ???
+ */
 void actMotor(uint16_t x);
 
 #endif
