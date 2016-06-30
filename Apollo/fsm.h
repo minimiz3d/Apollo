8e065eb22b7bc40fee5785f0ac2916da4d60d1be
minimiz3d <arthur-357@hotmail.com>
2016-06-29 22:26:41 -0300

    Comments

--- Apollo/fsm.h
+++ Apollo/fsm.h
@@ -15,7 +15,6 @@
  *	Cada uma destas variáveis será utilizada dentro de outros códigos fonte.
  *
  */
-/* Variáveis utilizadas */
 extern uint8_t mode;
 extern uint8_t done;
 
@@ -23,7 +22,6 @@ extern uint8_t done;
 /**
  *	@brief Enumeração de todos os possíveis estados da FSM.
  */
-/* Todos estados utilizados */
 typedef enum {
 	S0, S1, S2, S3, S4, S5, S6, S7, S8
 } States;
@@ -32,13 +30,11 @@ typedef enum {
 /**
  *	@brief Ponteiro de função para um determinado estado.
  */
-/* Ponteiro de função para um determinado estado */
 typedef void (*Action)();
 
 /**
  *	@brief Declaração da FSM com os campos necessários.
  */
-/* Dados e ponteiros de funções */
 typedef struct FSM {
 	States state;
 	Action action[15];
@@ -53,13 +49,11 @@ extern volatile fsm sm;
 /**
  *	@brief Inicializa a FSM com as funções de todos estados possíveis.
  */
-/* Inicialização da FSMs */
 void initFSM();
 
 /**
  *	@brief Definição de cada função dos estados inicias.
  */
-/* Estados iniciais */
 void s0();
 void s1();
 void s2();
@@ -67,7 +61,6 @@ void s2();
 /**
  *	@brief Definição de cada função de estado do modo de afinação.
  */
-/* Tuning */
 void t_s3();
 void t_s4();
 void t_s5();
@@ -78,7 +71,6 @@ void t_s8();
 /**
  *	@brief Definição de cada função de estado do modo de aprendizado.
  */
-/* Learning */
 void l_s3();
 void l_s4();
 void l_s5();
